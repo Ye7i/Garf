5778d7a3289073fb7191857fa907b05d8b9555c5
Frank Allenby <fallenby@live.com>
2013-08-29 09:20:00 +0200

    Added licence to source files.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,31 @@
-/* Greetings all who dare to tread here.
+/*	Garf IRC bot
+    Copyright (C) 2013  fallenby
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA. 
+    
+    Contact me(the founding developer) at:
+    fallenby@outlook.com */
+
+/* Greetings all..
  * Originally written by Frank Allenby.
- * Based on Tyler Allen's, which can be found over at http://www.the-tech-tutorial.com/?p=1716
  * 
- * His work is likely to end up a shuddering, sputtering remnant of its original self. Can't say I mind that, though.
+ * This work is based on Tyler Allen's code, which can be found over 
+ * at http://www.the-tech-tutorial.com/?p=1716
  * 
  * If you do hang around; thank you. I hope that you don't cringe too many times.
- * If you do, though, please drop a comment on the project repo at https://github.com/Ye7i/Garf.
+ * If you do, though, please drop a comment on the project repo at https://github.com/fallenby/Garf.
  * If you're really, really nice, add a few lines of code or fix some stuff.
  */
 
